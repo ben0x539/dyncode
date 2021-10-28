c8b20495d2e31659ca39668df2365f9f2903912f
Benjamin Herr <git@1d6.org>
2021-10-28 00:01:23 -0700

    parse with syn instead
    wow this is so much easier

--- src/lib.rs
+++ src/lib.rs
@@ -1,68 +1,63 @@
 extern crate proc_macro;
-use proc_macro::{TokenTree, TokenStream};
-use proc_macro::TokenTree::*;
+use proc_macro::{TokenStream};
+use proc_macro2::{Span, Ident};
+use syn::{parse_macro_input, Token, Result as SynResult, LitStr, Error};
+use syn::parse::{Parse, ParseStream};
 
-use color_eyre::eyre::{bail, Result, Report};
+use color_eyre::eyre::{Result, Report};
+
+struct Input {
+	product_key: String,
+	license_key: String,
+}
+
+impl Parse for Input {
+	fn parse(input: ParseStream) -> SynResult<Self> {
+		let mut product_key = None;
+		let mut license_key = None;
+		while !input.is_empty() {
+			let ident: Ident = input.parse()?;
+			let field = match &ident.to_string()[..] {
+				"product_key" => &mut product_key,
+				"license_key" => &mut license_key,
+				_ => return Err(Error::new(ident.span(), "bad parameter")),
+			};
+			if field.is_some() {
+				return Err(Error::new(ident.span(), "duplicate parameter"));
+			}
+			input.parse::<Token![=]>()?;
+			let lit: LitStr = input.parse()?;
+			*field = Some(lit.value());
+
+			if input.is_empty() { break; }
+
+			input.parse::<Token![,]>()?;
+		}
+
+		let get_err = |s| move || Error::new(Span::call_site(), s);
+		let product_key = product_key
+			.ok_or_else(get_err("missing product_key parameter"))?;
+		let license_key = license_key
+			.ok_or_else(get_err("missing license_key parameter"))?;
+		Ok(Input { product_key, license_key })
+	}
+}
 
 #[proc_macro]
 pub fn activate_license(input: TokenStream) -> TokenStream {
+	let input = parse_macro_input!(input);
 	match activate_license_inner(input) {
 		Ok(tokens) => tokens,
 		Err(e) => panic!("{}", e.to_string()),
 	}
 }
 
-fn activate_license_inner(input: TokenStream) -> Result<TokenStream> {
+fn activate_license_inner(input: Input) -> Result<TokenStream> {
 	let rt = tokio::runtime::Runtime::new().unwrap();
-	let (product_key, license_key) = parse_keys(input)
-		.map_err(|_| Report::msg("Library activation not recognized. Please contact your account manager."))?;
+	let Input { product_key, license_key } = input;
 	let result = rt.block_on(get_code(&product_key, &license_key))?;
 	Ok(result.parse().
 		map_err(|e: proc_macro::LexError| Report::msg(e.to_string()))?)
-}
-
-fn get_ident(input: &mut impl Iterator<Item=TokenTree>) -> Result<String> {
-	match input.next() {
-		Some(Ident(ident)) => Ok(ident.to_string()),
-		_ => bail!("bad ident"),
-	}
-}
-
-fn get_str_lit(input: &mut impl Iterator<Item=TokenTree>) -> Result<String> {
-	let s = match input.next() {
-		Some(Literal(literal)) => literal.to_string(),
-		_ => bail!("bad literal"),
-	};
-	if !s.starts_with("\"") {
-		bail!("bad literal");
-	}
-	Ok(s[1..s.len()-1].to_string())
-}
-
-fn want_punct(input: &mut impl Iterator<Item=TokenTree>, c: char)
-		-> Result<()> {
-	match input.next() {
-		Some(Punct(punct)) if punct.as_char() == c => Ok(()),
-		_ => bail!("bad punct"),
-	}
-}
-
-fn parse_keys(tokens: TokenStream) -> Result<(String, String)> {
-	let mut tokens = tokens.into_iter();
-
-	let first_ident = get_ident(&mut tokens)?;
-	want_punct(&mut tokens, '=')?;
-	let first_str_lit = get_str_lit(&mut tokens)?;
-	want_punct(&mut tokens, ',')?;
-	let second_ident = get_ident(&mut tokens)?;
-	want_punct(&mut tokens, '=')?;
-	let second_str_lit = get_str_lit(&mut tokens)?;
-
-	match (&first_ident.to_string()[..], &second_ident.to_string()[..]) {
-		("product_key", "license_key") => Ok((first_str_lit, second_str_lit)),
-		("license_key", "product_key") => Ok((second_str_lit, first_str_lit)),
-		_ => bail!("bad input 9"),
-	}
 }
 
 async fn get_code(product_key: &str, license_key: &str) -> Result<String> {
